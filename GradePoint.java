f2dfc9732a17dc6d9f1a293749668d75a435ce6a
Aritra Chowdhury <ac.is.working245@gmail.com>
2026-03-31 18:05:39 +0530

    Update GradePoint.java

--- GradePoint.java
+++ GradePoint.java
@@ -5,7 +5,7 @@
  *
  * Each grade stores its display label and the corresponding grade point value.
  *
- * Author: [Your Name]
+ * Author: Aritra Chowdhury
  * Course: Programming in Java - BYOP Project
  */
 public enum GradePoint {
